f7e8b32a383bacf4309c52cbc639eed434acca4c
Steve99-coder <stevenderitu99@gmail.com>
2021-01-09 15:32:18 +0300

    create class image

--- gallerypics/models.py
+++ gallerypics/models.py
@@ -5,3 +5,9 @@ import cloudinary
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Image(models.Model):
+    name =models.CharField(max_length = 30)
+    image_description = models.TextField()
+    image_location = models.ForeignKey('Location', on_delete=models.SET_NULL, default = '', null=True)
+    image_category = models.ForeignKey('Category', on_delete=models.CASCADE, default='')
+    image = CloudinaryField('image')

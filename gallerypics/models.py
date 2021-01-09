ad0feeddd020b1031a22fdfa15623470a806a041
Steve99-coder <stevenderitu99@gmail.com>
2021-01-09 15:29:25 +0300

    add requirements,txt dependencies

--- gallerypics/models.py
+++ gallerypics/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField
 
 # Create your models here.

b2b8b6309faa098bb4ddc7946208fd9caee3cc8f
Steve99-coder <stevenderitu99@gmail.com>
2021-01-09 15:39:01 +0300

    add class location

--- gallerypics/models.py
+++ gallerypics/models.py
@@ -11,3 +11,30 @@ class Image(models.Model):
     image_location = models.ForeignKey('Location', on_delete=models.SET_NULL, default = '', null=True)
     image_category = models.ForeignKey('Category', on_delete=models.CASCADE, default='')
     image = CloudinaryField('image')
+
+
+    def delete_image(self):
+        self.save()
+
+
+    @classmethod
+    def search_by_category(cls, image_category):
+        images = cls.objects.filter(image_category__category_name__icontains=image_category)
+        return images
+
+
+    def save_image(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    location_name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.location_name
+
+
+    def delete_location_name(self):
+        self.delete() 

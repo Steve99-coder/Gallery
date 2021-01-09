5918d0669dc65ad0aee203493e9552b96cbf66b0
Steve99-coder <stevenderitu99@gmail.com>
2021-01-09 17:10:34 +0300

    add redirect to views

--- gallerypics/views.py
+++ gallerypics/views.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-
+# -*- coding: utf-8 -*-from __future__ import unicode_literals
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse
+from .models import Image,Category,Location
 # Create your views here.

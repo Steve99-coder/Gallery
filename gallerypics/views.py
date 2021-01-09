b0a4e628a87f934fb9269832ea1ca6bea16c8cc7
Steve99-coder <stevenderitu99@gmail.com>
2021-01-09 17:13:00 +0300

    add photos .html file

--- gallerypics/views.py
+++ gallerypics/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from .models import Image,Category,Location
 # Create your views here.
+
+def image(request):
+    image = Image.objects.all()
+    image_location = Location.objects.all
+    return render(request, 'photos.html', {'image': image})

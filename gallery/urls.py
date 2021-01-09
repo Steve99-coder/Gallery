866b967def10573c0756a6c63bdb8ba1599a62e9
Steve99-coder <stevenderitu99@gmail.com>
2021-01-09 22:06:04 +0300

    add forms

--- gallery/urls.py
+++ gallery/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('galleryhub.urls')),
+    path('', include('gallerypics.urls')),
     
 ]
